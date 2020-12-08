4b7c454a4032bc35bd4c6df213f6c3c681060be7
Wojtek <waflu918@gmail.com>
2020-12-08 22:06:46 +0100

    fixed yaml parsing

--- src/app/components/api-definition-text-area/api-definition-text-area.component.ts
+++ src/app/components/api-definition-text-area/api-definition-text-area.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {YamlParserService} from '../../services/yaml-parser.service';
 import {Store} from '@ngrx/store';
 import {parseSpecification} from '../../state/specification.actions';
 import SpecificationState from '../../state/specification.state';
@@ -12,10 +11,7 @@ import {v4 as uuidv4} from 'uuid';
 })
 export class ApiDefinitionTextAreaComponent implements OnInit, OnDestroy {
 
-  routeUuid: string;
-
-  constructor(private readonly store: Store<SpecificationState>,
-              private yamlParser: YamlParserService) {
+  constructor(private readonly store: Store<SpecificationState>) {
   }
 
   ngOnInit() {
@@ -24,9 +20,8 @@ export class ApiDefinitionTextAreaComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
   }
 
-  submitSpec(yamlSpecification: string): void {
-    const specification = this.yamlParser.parseYamlSpec(yamlSpecification);
-    this.store.dispatch(parseSpecification({specification}));
+  submitSpec(yamlSpecification: any): void {
+    this.store.dispatch(parseSpecification({specification: yamlSpecification}));
   }
 
   generateUuid(): string {
