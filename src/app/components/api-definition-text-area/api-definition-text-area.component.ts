3c15497c61914d84e8530d7677619317594560c8
Wojtek <waflu918@gmail.com>
2020-12-09 20:03:22 +0100

    temporary mock of specification

--- src/app/components/api-definition-text-area/api-definition-text-area.component.ts
+++ src/app/components/api-definition-text-area/api-definition-text-area.component.ts
@@ -14,6 +14,68 @@ export class ApiDefinitionTextAreaComponent implements OnInit, OnDestroy {
   constructor(private readonly store: Store<SpecificationState>) {
   }
 
+  mock = "coapi: 1.0.0\n" +
+    "servers:\n" +
+    "  - url: iotdevices.com\n" +
+    "    description: Production server of IOT devices\n" +
+    "    port: 5356\n" +
+    "info:\n" +
+    "  title: Example IoT API\n" +
+    "  description: Example for PDI2. API contains ... some IoT endpoints representing hardware.\n" +
+    "  contact:\n" +
+    "    name: Wojciech Jenczalik\n" +
+    "    url: example.homepage.pl\n" +
+    "    email: jenczalik@pw.edu.pl\n" +
+    "  license:\n" +
+    "    name: MIT License\n" +
+    "    url: mit.edu/license.md\n" +
+    "  version: 1.0-SNAPSHOT\n" +
+    "  coapVersion: \"1\"\n" +
+    "paths:\n" +
+    "  - url: /lamps\n" +
+    "    description: Lamps endpoint - allowing to get current state of lamps.\n" +
+    "    get:\n" +
+    "      description: Get state of all lamps.\n" +
+    "  - url: /lamps/{lampId} \n" +
+    "    description: Endpoint allowing to turn on/off given lamp or get its current state.\n" +
+    "    get:\n" +
+    "      description: Get state of given lamp.\n" +
+    "      observable: true\n" +
+    "      etag: true\n" +
+    "    put:\n" +
+    "      description: Turns on/off the lamp.\n" +
+    "      payload: \n" +
+    "        description: Contains desired state of the lamp.\n" +
+    "        content:\n" +
+    "          - format: \"44\"\n" +
+    "            schema: JSON Schema 2019-09\n" +
+    "            examples:\n" +
+    "              - description: Example of turning the lamp on by JSON request.\n" +
+    "                value: \"{state: 1}\"\n" +
+    "          - format: \"0\"\n" +
+    "            schema: YAML Schema\n" +
+    "            examples:\n" +
+    "              - description: Example of turning the lamp off by YAML request.\n" +
+    "                value: \"state: 0\"\n" +
+    "        required: true\n" +
+    "      responses:\n" +
+    "        - code: default\n" +
+    "          description: A default response.\n" +
+    "          content:\n" +
+    "            - format: \"44\"\n" +
+    "              schema: JSON Schema 2019-09\n" +
+    "              examples:\n" +
+    "                - value: \"{state: 1}\"\n" +
+    "        - code: \"4.04\"\n" +
+    "          description: Not found.\n" +
+    "          content:\n" +
+    "            - format: \"0\"\n" +
+    "              schema: none\n" +
+    "              examples:\n" +
+    "                - value: \"Unable to find a lamp of given ID.\"\n" +
+    "    delete:\n" +
+    "      description: Removes given lamp from an API.";
+
   ngOnInit() {
   }
 
